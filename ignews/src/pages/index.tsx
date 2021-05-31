a86998c372af251f9f1134f43580612be1e8a7a4
JonathanLopesM <66283955+JonathanLopesM@users.noreply.github.com>
2021-05-31 11:10:57 -0300

    Estrutura da aplicação
    Estrutura e estilos iniciais prontos para inicio de qualquer aplicação Nextjs

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,20 @@
+
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <main >
+        <Head>
+                <title>Inicio | igNews</title>
+        </Head>
+        <h1 >
+          Hello World
+        </h1>
+      </main>
+
+      
+    </>
+  )
+}

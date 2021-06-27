8a03fe9bd4c0814040296c026554f1d9d076401c
JonathanLopesM <66283955+JonathanLopesM@users.noreply.github.com>
2021-06-27 12:07:44 -0300

    completo

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,20 +1,23 @@
 
 import {FaGithub} from 'react-icons/fa';
-import {FiX} from 'react-icons/fi'
+import {FiX} from 'react-icons/fi';
+import {signIn, signOut, useSession} from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SignInButton(){
-  const isUserLoggedIn = true;
+  const [session] = useSession() //se o user esta logado ou não
 
-  return isUserLoggedIn ? (
+
+  return session ? (//se esta logado
           
     <button 
     type="button"
     className={styles.signInButton}
+    onClick={() => signOut()}
     >
       <FaGithub color= "#04d361"/>
-      Jonathan Lopes M
+      {session.user.name}
       <FiX color = "#737388" className={styles.closeIcon}/>
 </button>
   ) : (
@@ -22,6 +25,7 @@ export function SignInButton(){
     <button 
     type="button"
     className={styles.signInButton}
+    onClick={() => signIn('github')}
     >
       <FaGithub color= "#eba417"/>
       Sign in with GitHub

82c790f7f10eba3f2e84f67dad0e674f480e6982
Jonathan Lopes Machado <jonathan.lopes1004@gmail.com>
2022-06-21 11:46:54 -0300

    test
    ignews

--- ignews/src/pages/posts/preview/[slug].tsx
+++ ignews/src/pages/posts/preview/[slug].tsx
@@ -1,16 +1,11 @@
-import { GetStaticPaths, GetStaticProps } from "next";
-import Link from 'next/link'
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/client";
 import Head from "next/head";
 import { RichText } from "prismic-dom";
+import getPrismicClient from "../../../services/prismic";
+import styles from './post.module.scss';
 
-import { getPrismicClient } from "../../../services/prismic";
-
-import styles from '../post.module.scss';
-import { useSession } from "next-auth/client";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
-interface PostPreviewProps {
+interface PostProps {
   post: {
     slug: string;
     title: string;
@@ -19,6 +14,7 @@ interface PostPreviewProps {
   }
 }
 
+<<<<<<< Updated upstream
 export default function PostPreview({ post }: PostPreviewProps) {
   const [session] = useSession()
   const router = useRouter()
@@ -51,28 +47,61 @@ export default function PostPreview({ post }: PostPreviewProps) {
           </div>
         </article>
       </main>
+=======
+export default function Post({post}: PostProps) {
+  return (
+    <>
+    
+    <Head>
+      <title>{post.title} | Hnews</title>
+    </Head>
+
+    <main className={styles.container}>
+      <article className={styles.post}>
+        <h1>{post.title}</h1>
+        <time>{post.updatedAt}</time>
+        <div
+          className={styles.postContent}
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />  
+      </article>  
+    </main> 
+    
+>>>>>>> Stashed changes
     </>
   );
 }
 
-export const getStaticPaths : GetStaticPaths = async () => {
-  return { //geração estatica definida
-    paths : [],
-    fallback : 'blocking'//true //false //'blocking'
-  }
-}
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  const session = await getSession({ req });
   const { slug } = params;
 
-  const prismic = getPrismicClient()
+  if (!session?.activeSubscription) {
+    return {
+      redirect: { 
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
+  const prismic = getPrismicClient(req);
+
+<<<<<<< Updated upstream
   const response = await prismic.getByUID('posts', String(slug), {})
 
   const post = {
     slug,
     title: response.data.title,
     content: RichText.asHtml(response.data.content.splice(0, 3)),
+=======
+  const response = await prismic.getByUID<any>('posts', String(slug), {});
+
+  const post = {
+    slug, 
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+>>>>>>> Stashed changes
     updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
       day: '2-digit',
       month: 'long',
@@ -81,9 +110,14 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   };
 
   return {
-    props: {
+    props: { 
       post,
+<<<<<<< Updated upstream
     },
     redirect: 60 * 30, //tempo para revalidado 30min
+=======
+    }
+>>>>>>> Stashed changes
   }
+
 }
